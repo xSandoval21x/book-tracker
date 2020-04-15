76eba32ed543cf96f2628c63b0fc40a422834fdf
esandoval <xsandoval21x@gmail.com>
2020-04-14 18:24:01 -0700

    Add remove row button and change read status button

--- books.js
+++ books.js
@@ -18,22 +18,52 @@ function addToLibrary(newBook) {
 function render(newBook) {
     const table = document.querySelector("table");
     const newRow = table.insertRow();
+    const deleteCell = newRow.insertCell();
     const titleCell = newRow.insertCell();
     const authorCell = newRow.insertCell();
     const pagesCell = newRow.insertCell();
     const haveReadCell = newRow.insertCell();
+    const changeStatusCell = newRow.insertCell();
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "\u2212";
+    deleteCell.appendChild(deleteButton);
+    deleteButton.addEventListener("click", deleteRow);
+
+    const changeStatusButton = document.createElement("button");
+    if(newBook.haveRead){
+        changeStatusButton.innerText = "\u2718";
+    }else {
+        changeStatusButton.innerText = "\u2714";
+    }
+    changeStatusCell.appendChild(changeStatusButton);
+    changeStatusButton.addEventListener("click", function() {
+        newBook.haveRead = !newBook.haveRead;
+        if(newBook.haveRead){
+            changeStatusButton.innerText = "\u2718";
+        }else {
+            changeStatusButton.innerText = "\u2714";
+        }
+        haveReadCell.innerText = newBook.haveRead? "Read" : "Not read";
+    });
 
     titleCell.innerText = newBook.name;
     authorCell.innerText = newBook.author;
-    pagesCell.innerText = newBook.pages;
-    haveReadCell.innerText = newBook.haveRead;
+    pagesCell.innerText = newBook.pages
+    haveReadCell.innerText = newBook.haveRead? "Read" : "Not read";
 }
+
+function deleteRow() {
+    const row = event.target.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
 
 function newEntry() {
     const newTitle = document.getElementById("title").value;
     const newAuthor = document.getElementById("author").value;
     const newPages = document.getElementById("pages").value;
-    const newReadStatus = document.getElementById("have-read").checked? "Read" : "Not read";
+    const newReadStatus = document.getElementById("have-read").checked? true : false;
 
     if(newTitle === "" || newAuthor === "" || newPages === "") {
         alert("Check missing fields!");
@@ -70,8 +100,8 @@ function toggleVisibility() {
     bookInfo.classList.toggle("visible");
 }
 
-addToLibrary(new Book("The Great Gatsby", "F. Scott Fitzgerald", "218", "Not read"));
-addToLibrary(new Book("War and Peace", "Leo Tolstoy", "1,225", "Not read"));
+addToLibrary(new Book("The Great Gatsby", "F. Scott Fitzgerald", "218", false));
+addToLibrary(new Book("War and Peace", "Leo Tolstoy", "1,225", false));
 
 submitButton.addEventListener("click", newEntry);
 addEntryButton.addEventListener("click", toggleVisibility);
